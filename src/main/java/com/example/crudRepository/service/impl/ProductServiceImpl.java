3dc231d994c30dc0c9c22e8d6e70a4b8cdddfb5a
Naresh-Kumar-Kotha98 <targetnareshk@gmail.com>
2025-09-08 17:50:17 +0530

    singleTon DesignPatten1

--- src/main/java/com/example/crudRepository/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/crudRepository/service/impl/ProductServiceImpl.java
@@ -14,6 +14,7 @@ import com.example.crudRepository.model.Product;
 import com.example.crudRepository.model.dto.ProductResponse;
 import com.example.crudRepository.repository.ProductRepository;
 import com.example.crudRepository.service.ProductService;
+import com.example.util.SingleTonCls;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -30,6 +31,10 @@ public class ProductServiceImpl implements ProductService{
 	})
 	public ProductResponse saveProduct(Product product) {
 		System.out.println("create product");
+		SingleTonCls sIngleTonCls = SingleTonCls.getInstance();
+		SingleTonCls sIngleTonCls2 = SingleTonCls.getInstance();
+		sIngleTonCls.staticMethDisplay();
+		sIngleTonCls.normalMethDisplay();
 		Product savedProduct = productRepository.save(product);
 		ProductResponse productResponse = new ProductResponse(savedProduct);
 		return productResponse;
