f61be178c737cc23e34e283c06e7491d7118114d
Naresh-Kumar-Kotha98 <targetnareshk@gmail.com>
2025-09-30 22:14:42 +0530

    code formatting

--- src/main/java/com/example/crudRepository/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/crudRepository/service/impl/ProductServiceImpl.java
@@ -18,65 +18,60 @@ import com.example.crudRepository.repository.ProductRepository;
 import com.example.crudRepository.service.ProductService;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
-	private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-	@Autowired
-	public ProductServiceImpl(ProductRepository productRepository) {
-		this.productRepository = productRepository;
-	}
+  @Autowired
+  public ProductServiceImpl(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
-	@Caching(evict = {
-			@CacheEvict(cacheNames = "cachedProducts", allEntries = true)
-	})
-	public ProductResponse saveProduct(Product product) {
-		System.out.println("create product");
-		Product savedProduct = productRepository.save(product);
-		ProductResponse productResponse = new ProductResponse(savedProduct);
-		return productResponse;
-	}
-	
-	@Caching(evict = {
-			@CacheEvict(cacheNames = "cachedProducts", allEntries = true)
-	})
-	@CachePut(cacheNames = "cachedProduct", key = "#id")
-	public Product updateProduct(Product product, String id) {
-		System.out.println("updateing in db:" + id);
+  @Caching(evict = { @CacheEvict(cacheNames = "cachedProducts", allEntries = true) })
+  public ProductResponse saveProduct(Product product) {
+    System.out.println("create product");
+    Product savedProduct = productRepository.save(product);
+    ProductResponse productResponse = new ProductResponse(savedProduct);
+    return productResponse;
+  }
+
+  @Caching(evict = { @CacheEvict(cacheNames = "cachedProducts", allEntries = true) })
+  @CachePut(cacheNames = "cachedProduct", key = "#id")
+  public Product updateProduct(Product product, String id) {
+    System.out.println("updateing in db:" + id);
 //		productRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Item not found with id: " + id));
-		Product savedProduct = productRepository.save(product);
-		return savedProduct;
-	}
+    Product savedProduct = productRepository.save(product);
+    return savedProduct;
+  }
 
-	@Cacheable(cacheNames = "cachedProducts")
-	public List<Product> getProducts() {
-		System.out.println("searching in db for all products.");
-		List<Product> products = productRepository.findAll();
-		return products;
-	}
-	
+  @Cacheable(cacheNames = "cachedProducts")
+  public List<Product> getProducts() {
+    System.out.println("searching in db for all products.");
+    List<Product> products = productRepository.findAll();
+    return products;
+  }
+
 //	@Cacheable(cacheNames = "cachedProductsByPage")
-	public Page<Product> getProductsByPageSize(Pageable pageable) {
-		System.out.println("searching in db for all products by pagenumber.");
-		Page<Product> products = productRepository.findAll(pageable);
-		return products;
-	}
+  public Page<Product> getProductsByPageSize(Pageable pageable) {
+    System.out.println("searching in db for all products by pagenumber.");
+    Page<Product> products = productRepository.findAll(pageable);
+    return products;
+  }
 
-	@Cacheable(cacheNames = "cachedProduct", key = "#id")
-	public Product getProduct(String id) {
-		System.out.println("get product with id:"+ id);
-		Product product = productRepository.findById(id).orElseThrow(() ->
-				new ResourceNotFoundException("No product found with id:" + id));
-		return product;
-	}
+  @Cacheable(cacheNames = "cachedProduct", key = "#id")
+  public Product getProduct(String id) {
+    System.out.println("get product with id:" + id);
+    Product product = productRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("No product found with id:" + id));
 
-	@Caching(evict = {
-			@CacheEvict(cacheNames = "cachedProducts", allEntries = true),
-			@CacheEvict(cacheNames = "cachedProduct", key = "#id")
-	})
-	public void deleteProduct(String id) {
-		System.out.println("delete product with id:"+ id);
+    return product;
+  }
+
+  @Caching(evict = { @CacheEvict(cacheNames = "cachedProducts", allEntries = true),
+      @CacheEvict(cacheNames = "cachedProduct", key = "#id") })
+  public void deleteProduct(String id) {
+    System.out.println("delete product with id:" + id);
 //		productRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Item not found with id: " + id));
-		productRepository.deleteById(id);
-	}
+    productRepository.deleteById(id);
+  }
 }
